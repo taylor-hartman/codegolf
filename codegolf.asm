8cf11a32f834ec932b8ea919882d44b1480b345d
taylor hartman <sussytech@gmail.com>
2024-03-04 15:46:28 -0600

    negative line

--- codegolf.asm
+++ codegolf.asm
@@ -25,25 +25,41 @@ start:
 	mov al, 0x07
 	call clear_screen
 
-mov di, 320*50+120
-mov cx, 80
-call draw_horizontal_line
-mov di, 320*50+120
-mov cx, 80
-call draw_vertical_line
-mov di, 320*130+120
-mov cx, 80
-call draw_horizontal_line
-mov di, 320*50+200
-mov cx, 80
-call draw_vertical_line
+call sign_handler
+;mov al, 0x03
+;mov di, [points]
+;movzx cx, [lens]
+;call draw_horizontal_line_negative
+;mov di, [points + 2]
+;mov cx, [lens + 1]
+;call draw_vertical_line_negative
 jmp exit
 
+sign_handler: ;cx=sign
+	mov al, 0x03
+	mov di, [points]
+	movzx cx, [signs]
+	jcxz positive  ;jmp if cx==0
+	negative:
+	movzx cx, [lens] 
+	call draw_horizontal_line_negative
+	ret
+	positive:
+	movzx cx, [lens]
+	call draw_horizontal_line
+	ret
+	
 draw_horizontal_line: ;di=start, cx=len
 	mov al, 0x03
 	rep stosb
 	ret
 
+draw_horizontal_line_negative:
+	mov [es:di], al
+	dec di
+	loop draw_horizontal_line_negative
+	ret
+	
 draw_vertical_line: ;di=start, cx=len
 	mov [es:di], al
 	dec cx
@@ -51,7 +67,15 @@ draw_vertical_line: ;di=start, cx=len
 	cmp cx, 0
 	jne draw_vertical_line
 	ret
-	
+
+draw_vertical_line_negative:
+	mov [es:di], al
+	dec cx
+	add di, 320
+	cmp cx, 0
+	jne draw_vertical_line_negative
+	ret
+
 draw_pixel: ;al=color, bx=y, cx=x
 	mov di, 320
 	imul di, bx
@@ -68,6 +92,15 @@ clear_screen:
 exit:
 	jmp exit
 
+points:
+	dw 50*320+190, 50*320+190
+
+lens:
+	db 140, 40
+
+signs: 
+	db 1,0
 
 times 510-($-$$) db 0
 dw 0xAA55
+	
