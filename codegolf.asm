525a6697f39e3c57d888c2112de700da19688165
taylor hartman <sussytech@gmail.com>
2024-03-04 20:29:49 -0600

    ball

--- codegolf.asm
+++ codegolf.asm
@@ -9,7 +9,7 @@ mov sp, ax
 ;point es to video memory
 mov ax, 0xA000
 mov es, ax
-
+mov ah, 0x01
 ;switch to video mode 0x13
 mov ah, 0x00
 mov al, 0x13
@@ -31,6 +31,10 @@ mov si, lens
 call draw_series_hstart
 mov di, [points]
 call draw_series_vstart
+mov al, 0x0f
+mov bx, 40
+mov cx, 60
+call draw_ball
 jmp exit
 
 draw_series_hstart:
@@ -61,11 +65,32 @@ draw_vertical_line: ;di=start, cx=len
 	jne draw_vertical_line
 	ret
 
-draw_pixel: ;al=color, bx=y, cx=x
+draw_ball: ;al=color, bx=y, cx=x
+	mov al, 0x0f
 	mov di, 320
 	imul di, bx
-	add di, cx	
+	add di, cx
+	push cx	
+	cmp di, [es:di]
+	jne skip
+	
+skip:	mov [es:di], al
+	mov cx, [0x046c]
+	inc cx
+	call delay
+	pop cx
+	mov al, 0x00
 	mov [es:di], al
+	inc cx
+	jmp draw_ball	
+
+mov_ball:
+	mov ax, -4
+
+delay:
+	cmp [0x046c], cx
+	jb delay
+	ret
 	
 clear_screen:
 	mov cx, 320*200
